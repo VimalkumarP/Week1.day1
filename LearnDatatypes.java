9d89af90b33026457fc83536a6399c25e14d1638
Vimalkumar P <88431740+VimalkumarP@users.noreply.github.com>
2022-04-16 15:25:06 +0530

    Add files via upload

---
+++ LearnDatatypes.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class LearnDatatypes {
+
+	
+     //  Primitive Datatypes - Car;
+	// Datatype VariableName = Value;
+	
+		byte carSeries = 12;
+		short cubicCapacity = 1999;
+		int chasisNumber = 984512367;
+		long engineNumber = 9876543210L;
+		float milage = 23.56987F;
+		double milageForHundredLitre = 2300.74686684;
+		char varient = 'S';
+		boolean isfueled = true;
+		
+		
+	
+
+}
